5cfedc47c553fd9d0a7b63c23e5c1cb1c5ca6c48
Dominik Voda <d.voda94@gmail.com>
2019-02-17 18:44:26 +0100

    #5 + add post condition trait rule for mockery expectations

---
+++ src/BrandEmbassyCodingStandard/PhpStan/Rules/Mockery/PostConditionsTraitUsedRule.php
@@ -0,0 +1,91 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassyCodingStandard\PhpStan\Rules\Mockery;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleLevelHelper;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+use function in_array;
+use function sprintf;
+
+class PostConditionsTraitUsedRule implements Rule
+{
+    private static $callCountMethods = [
+        'zero',
+        'once',
+        'twice',
+        'times',
+        'zeroOrMoreTimes',
+        'atLeast',
+        'never',
+        'atMost',
+        'between',
+    ];
+
+    /**
+     * @var RuleLevelHelper
+     */
+    private $ruleLevelHelper;
+
+
+    public function __construct(RuleLevelHelper $ruleLevelHelper)
+    {
+        $this->ruleLevelHelper = $ruleLevelHelper;
+    }
+
+
+    public function getNodeType(): string
+    {
+        return MethodCall::class;
+    }
+
+
+    /**
+     * @param MethodCall $node
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if (!$node->name instanceof Node\Identifier) {
+            return [];
+        }
+
+        $name = $node->name->name;
+
+        if (!in_array($name, self::$callCountMethods, true)) {
+            return [];
+        }
+
+        $typeResult = $this->ruleLevelHelper->findTypeToCheck(
+            $scope,
+            $node->var,
+            sprintf('Call to method %s() on an unknown class %%s.', $name),
+            static function (Type $type) use ($name): bool {
+                return $type->canCallMethods()->yes() && $type->hasMethod($name)->yes();
+            }
+        );
+
+        /** @var ObjectType $type */
+        $type = $typeResult->getType();
+
+        if ($type->getClassName() !== 'Mockery\\Expectation') {
+            return [];
+        }
+
+        $classReflection = $scope->getClassReflection();
+
+        if($classReflection === null){
+            return [];
+        }
+
+        $traitName = 'Mockery\\Adapter\\Phpunit\\MockeryPHPUnitIntegration';
+        if (!$classReflection->hasTraitUse($traitName)) {
+            return [sprintf('Calling %s without %s trait.', $name, $traitName)];
+        }
+
+        return [];
+    }
+}
