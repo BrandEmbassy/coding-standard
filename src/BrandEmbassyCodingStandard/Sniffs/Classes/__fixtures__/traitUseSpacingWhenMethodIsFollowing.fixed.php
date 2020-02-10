4f61542a875336ccb060ebf922a43bb252fd8450
Dominik Voda <d.voda94@gmail.com>
2020-02-10 13:53:03 +0100

    + trait spacing sniff including option to define spaces before following property/constant (#40)
    * + trait spacing sniff including option to define spaces before following property/constant (#39)
    
    (cherry picked from commit 294fb0fbe0f7310aebf3215ae6be6c90ece1c0d8)
    
    * @ fix phpscs

---
+++ src/BrandEmbassyCodingStandard/Sniffs/Classes/__fixtures__/traitUseSpacingWhenMethodIsFollowing.fixed.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassyCodingStandard\Sniffs\Classes\__fixtures__;
+
+use Bar;
+
+final class FooBarClass
+{
+    use Bar;
+
+
+    public function foo(): string
+    {
+        return 'foo';
+    }
+}
