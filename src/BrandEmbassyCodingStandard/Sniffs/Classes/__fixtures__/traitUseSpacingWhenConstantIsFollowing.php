294fb0fbe0f7310aebf3215ae6be6c90ece1c0d8
Dominik Voda <d.voda94@gmail.com>
2020-02-10 13:22:24 +0100

    + trait spacing sniff including option to define spaces before following property/constant (#39)

---
+++ src/BrandEmbassyCodingStandard/Sniffs/Classes/__fixtures__/traitUseSpacingWhenConstantIsFollowing.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassyCodingStandard\Sniffs\Classes\__fixtures__;
+
+use Bar;
+
+final class FooBarClass
+{
+    use Bar;
+    private const FOO = 'foo';
+}
