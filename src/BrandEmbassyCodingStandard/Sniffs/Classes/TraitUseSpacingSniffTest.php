275325a7841c0605aa198c4d934f5f084d170f1a
Dominik Voda <d.voda94@gmail.com>
2020-02-10 13:22:24 +0100

    + trait spacing sniff including option to define spaces before following property/constant (#39)
    (cherry picked from commit 294fb0fbe0f7310aebf3215ae6be6c90ece1c0d8)

---
+++ src/BrandEmbassyCodingStandard/Sniffs/Classes/TraitUseSpacingSniffTest.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassyCodingStandard\Sniffs\Classes;
+
+use PHPUnit\Framework\Assert;
+use SlevomatCodingStandard\Sniffs\TestCase;
+
+final class TraitUseSpacingSniffTest extends TestCase
+{
+    public function testFixWhenMethodIsFollowing(): void
+    {
+        $report = self::checkFile(
+            __DIR__ . '/__fixtures__/traitUseSpacingWhenMethodIsFollowing.php',
+            $this->getSniffProperties()
+        );
+
+        Assert::assertSame(1, $report->getErrorCount());
+
+        self::assertSniffError($report, 9, TraitUseSpacingSniff::CODE_INCORRECT_LINES_COUNT_AFTER_LAST_USE);
+
+        self::assertAllFixedInFile($report);
+    }
+
+
+    public function testFixWhenConstantIsFollowing(): void
+    {
+        $report = self::checkFile(
+            __DIR__ . '/__fixtures__/traitUseSpacingWhenConstantIsFollowing.php',
+            $this->getSniffProperties()
+        );
+
+        Assert::assertSame(1, $report->getErrorCount());
+
+        self::assertSniffError($report, 9, TraitUseSpacingSniff::CODE_INCORRECT_LINES_COUNT_AFTER_LAST_USE);
+
+        self::assertAllFixedInFile($report);
+    }
+
+
+    public function testFixWhenPropertyIsFollowing(): void
+    {
+        $report = self::checkFile(
+            __DIR__ . '/__fixtures__/traitUseSpacingWhenConstantIsFollowing.php',
+            $this->getSniffProperties()
+        );
+
+        Assert::assertSame(1, $report->getErrorCount());
+
+        self::assertSniffError($report, 9, TraitUseSpacingSniff::CODE_INCORRECT_LINES_COUNT_AFTER_LAST_USE);
+
+        self::assertAllFixedInFile($report);
+    }
+
+
+    /**
+     * @return array<int, int>|int[]
+     */
+    private function getSniffProperties(): array
+    {
+        return [
+            'linesCountBetweenUses' => 0,
+            'linesCountBeforeFirstUse' => 0,
+            'linesCountAfterLastUse' => 2,
+            'linesCountAfterLastUseWhenLastInClass' => 0,
+            'linesBeforeFollowingPropertyOrConstant' => 1,
+        ];
+    }
+}
