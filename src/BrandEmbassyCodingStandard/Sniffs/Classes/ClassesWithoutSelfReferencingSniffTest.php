65bce6bfa900762fef6bbbf9ffd6a10220e32bd7
Dominik Voda <d.voda94@gmail.com>
2021-09-16 15:08:56 +0200

    Add ClassesWithoutSelfReferencing sniff (#49)

---
+++ src/BrandEmbassyCodingStandard/Sniffs/Classes/ClassesWithoutSelfReferencingSniffTest.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassyCodingStandard\Sniffs\Classes;
+
+use PHPUnit\Framework\Assert;
+use RuntimeException;
+use SlevomatCodingStandard\Sniffs\TestCase;
+
+final class ClassesWithoutSelfReferencingSniffTest extends TestCase
+{
+    public function testFixFile(): void
+    {
+        $file = __DIR__ . '/__fixtures__/classesWithoutSelfReferencing.php';
+        require_once $file;
+
+        $report = self::checkFile($file, ['classesWithoutSelfReferencing' => [Assert::class]]);
+
+        Assert::assertSame(3, $report->getErrorCount());
+        self::assertSniffError(
+            $report,
+            16,
+            'ClassesWithoutSelfReferencing',
+            'Using self::assertFalse is forbidden. Call PHPUnit\Framework\Assert::assertFalse directly.'
+        );
+        self::assertSniffError(
+            $report,
+            17,
+            'ClassesWithoutSelfReferencing',
+            'Using static::assertEquals is forbidden. Call PHPUnit\Framework\Assert::assertEquals directly.'
+        );
+        self::assertSniffError(
+            $report,
+            19,
+            'ClassesWithoutSelfReferencing',
+            'Using static::assertTrue is forbidden. Call PHPUnit\Framework\Assert::assertTrue directly.'
+        );
+
+        self::assertAllFixedInFile($report);
+        Assert::assertSame(3, $report->getFixedCount());
+    }
+
+
+    public function testNoOptionProvided(): void
+    {
+        $file = __DIR__ . '/__fixtures__/classesWithoutSelfReferencing.php';
+        require_once $file;
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('The option "classesWithoutSelfReferencing" was not provided.');
+
+        self::checkFile($file);
+    }
+}
---
+++ src/BrandEmbassyCodingStandard/Sniffs/Classes/__fixtures__/classesWithoutSelfReferencing.fixed.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types = 1);
+
+namespace Foo\Bar;
+
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+final class FooFooFoo extends TestCase
+{
+    private static $lol;
+
+    public function barBarBar(): void
+    {
+        self::$lol = 'A';
+        Assert::assertTrue(true);
+        \PHPUnit\Framework\Assert::assertFalse(false);
+        \PHPUnit\Framework\Assert::assertEquals(1, 2);
+        self::$lol = 'B';
+        \PHPUnit\Framework\Assert  ::
+        assertTrue(false);
+        self::lolSelf();
+        static::lolStatic();
+        self::any();
+        self::$lol = 'C';
+    }
+
+
+    private static function lolStatic(): void
+    {
+        new static();
+    }
+
+
+    private
+    static
+    function lolSelf(): void
+    {
+        new
+        self();
+    }
+}
