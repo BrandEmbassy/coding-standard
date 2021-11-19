441b2d17f24875a3b1e590e39888d0527ce73397
Petr Heinz <petr.heinz@niceincontact.com>
2021-11-19 14:22:40 +0100

    Add PhpStan rule to ensure all tests are executed

---
+++ src/BrandEmbassyCodingStandard/PhpStan/Rules/Method/__fixtures__/NonPhpUnitTest.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassyCodingStandard\PhpStan\Rules\Method\__fixtures__;
+
+use PHPUnit\Framework\Assert;
+
+final class NonPhpUnitTest
+{
+    public function testFunctionPublic(): void
+    {
+        Assert::assertTrue(true);
+    }
+
+
+    protected function testFunctionProtected(): void
+    {
+        Assert::assertTrue(true);
+    }
+
+
+    private function testFunctionPrivate(): void
+    {
+        Assert::assertTrue(true);
+    }
+
+
+    public function nonTestFunction(): void
+    {
+        Assert::assertTrue(true);
+    }
+}
