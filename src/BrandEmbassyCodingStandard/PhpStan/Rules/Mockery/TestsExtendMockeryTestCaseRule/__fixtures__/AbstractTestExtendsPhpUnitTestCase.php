217a0334e8b1c0af75e2a6ee13c4e6db522d3150
dominikkaluza <dominik.kaluza@centrum.cz>
2025-12-17 14:26:34 +0100

    TestsExtendMockeryTestCaseRule: Include abstract classes in the rule

---
+++ src/BrandEmbassyCodingStandard/PhpStan/Rules/Mockery/TestsExtendMockeryTestCaseRule/__fixtures__/AbstractTestExtendsPhpUnitTestCase.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassyCodingStandard\PhpStan\Rules\Mockery\TestsExtendMockeryTestCaseRule\__fixtures__;
+
+use PHPUnit\Framework\TestCase;
+
+abstract class AbstractTestExtendsPhpUnitTestCase extends TestCase
+{
+}
